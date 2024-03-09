ec1d80954f2e9d00dd0e685d979ecce25ad670b0
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2024-03-08 18:04:00 -0600

    chore: release 1.0.2

--- lua/typescript-type-checker.lua
+++ lua/typescript-type-checker.lua
@@ -11,58 +11,64 @@ local M = {}
 M.config = config
 
 M.run = function()
+  local success, pcall_result = pcall(require, "fidget.progress")
+
+  local progress
+
+  if success then
+    progress = pcall_result
+  end
+
   local tsc_command = "npx tsc --noEmit --pretty false"
-  local issues_found = false -- Track whether any issues were found
 
-  local on_event = function(_, data, event)
-    if event == "stdout" or event == "stderr" then
-      local qf_list = {}
-      for _, line in ipairs(data) do
-        if line ~= "" then -- Ignore empty lines
-          local filename, lnum, col, err_type, err_code, message =
-            line:match("([^%(%)]+)%((%d+),(%d+)%)%:%s*(%w+)%s*(TS%d+):%s*(.*)")
-          if filename and lnum and col and message then
-            issues_found = true -- Set flag to true if an issue is found
-            local type = (err_type == "error" and "E") or (err_type == "warning" and "W") or ""
-            local formatted_message = err_type .. " " .. err_code .. ": " .. message
+  local handle = progress
+    and progress.handle.create({
+      title = "Type-checking",
+      message = "Running TypeScript type-checker...",
+      lsp_client = { name = "typescript-type-checker" },
+    })
 
-            table.insert(qf_list, {
-              filename = filename,
-              lnum = tonumber(lnum),
-              col = tonumber(col),
-              text = formatted_message,
-              type = type,
-            })
-          end
+  local on_exit = function()
+    if handle then
+      handle:finish()
+    end
+  end
+
+  local on_stdout = function(_, data)
+    local qf_list = {}
+
+    for _, line in ipairs(data) do
+      if line ~= "" then -- Ignore empty lines
+        -- Match the filename, line number, column number, and the full message
+        local filename, lnum, col, full_message = line:match("([^%(%)]+)%((%d+),(%d+)%)%:%s*(.*)")
+
+        if filename and lnum and col and full_message then
+          table.insert(qf_list, {
+            filename = filename,
+            lnum = tonumber(lnum),
+            col = tonumber(col),
+            text = full_message,
+            type = "E",
+          })
         end
       end
+    end
 
-      if #qf_list > 0 then
-        vim.fn.setqflist(qf_list, "r")
-        vim.cmd("copen")
-      end
-    elseif event == "exit" then
-      if not issues_found then
-        -- Clear the quickfix list and close the quickfix window if no issues were found
-        vim.fn.setqflist({}, "r")
-        vim.cmd("cclose")
-        vim.defer_fn(function()
-          print("No TypeScript issues found")
-        end, 0)
-      else
-        vim.defer_fn(function()
-          print("TypeScript check complete")
-        end, 0)
-      end
+    vim.fn.setqflist(qf_list, "r")
+
+    if #qf_list > 0 then
+      vim.cmd("copen")
+      vim.notify("Found " .. #qf_list .. " TypeScript errors", vim.log.levels.ERROR)
+    else
+      vim.cmd("cclose")
+      vim.notify("No TypeScript errors found", vim.log.levels.INFO)
     end
   end
 
   vim.fn.jobstart(tsc_command, {
-    on_stdout = on_event,
-    on_stderr = on_event,
-    on_exit = on_event,
+    on_stdout = on_stdout,
+    on_exit = on_exit,
     stdout_buffered = true,
-    stderr_buffered = true,
   })
 end
 
